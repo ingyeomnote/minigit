52e3323b3108522f5836ecaf954a6ac4ba2b9b4f
ingyeomnote <freeartjin@naver.com>
2024-09-26 00:00:37 +0900

    SpringBoot 초기 설정

---
+++ src/test/java/com/greencore/minigit/MinigitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greencore.minigit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinigitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
